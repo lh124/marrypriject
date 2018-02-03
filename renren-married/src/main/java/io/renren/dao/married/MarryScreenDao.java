c1b353f3941bb80b50e8537008a02258663a66e9
liaohuan <liaohuan@344193c1-dbe3-fc43-a527-1fd986941d42>
2018-02-03 03:05:27 +0000

    屏幕管理dao层

---
+++ renren-married/src/main/java/io/renren/dao/married/MarryScreenDao.java
@@ -0,0 +1,17 @@
+package io.renren.dao.married;
+
+import io.renren.dao.BaseDao;
+import io.renren.entity.married.MarryScreenEntity;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * 
+ * 
+ * @author yuanbichang
+ * @email 673290684@qq.com
+ * @date 2018-02-02 12:44:59
+ */
+public interface MarryScreenDao extends BaseDao<MarryScreenEntity>,BaseMapper<MarryScreenEntity> {
+	
+}
